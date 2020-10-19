d32badecdd751bcdcdd428c072217aa5b09e1355
Paddy McGlone <paddymcglone@gmail.com>
2020-10-19 19:38:47 +0100

    Adding curley braces for PS

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -24,9 +24,13 @@ namespace WishList
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }
             else
+            {
                 app.UseExceptionHandler("Home/Error");
+            }
 
             app.UseRouting();
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
